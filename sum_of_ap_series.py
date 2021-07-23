6bc97c4c013a57f82a4c112a56b65a65cf8cbbbb
Priyanka Banerjee <49121592+Priktopic@users.noreply.github.com>
2021-07-23 16:25:39 +0530

    Update sum_of_ap_series.py

--- sum_of_ap_series.py
+++ sum_of_ap_series.py
@@ -1,4 +1,6 @@
 def ap_sum(n,a,d): #n = length, a = first number, d = difference between numbers
     return n/2*(2*a+(n-1)*d)
 
+# Formula to find sum of an AP series is n/2*(2*a+(n-1)*d). If the series is 1,2,3,4,... the the formula is n*(n+1)/2
+
 print("Sum of the series:",ap_sum(50,2,2))
