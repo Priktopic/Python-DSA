8033520c1dfce88064d0f83013e091c42ade3c32
Priyanka Banerjee <49121592+Priktopic@users.noreply.github.com>
2021-07-23 17:23:13 +0530

    Update operations_on_dictionary.py

--- operations_on_dictionary.py
+++ operations_on_dictionary.py
@@ -30,23 +30,39 @@ courses = {
                      }
     }
     
-def which_book_taught(courses, teacher_name):
+def which_book_taught_in_sem(courses, teacher_name, course):
     books=[]
     semester=[]
+    """
+    Prints all the keys and values of the dictionary. 
+    Here the keys are "spring2020","fall2020" and "spring2021" and the remaining are values
+    """
     for sem, sub in courses.items():
+            
+        """
+        In "sub" part of the above dict there is another dict that contains keys as subjectnumber and its name along with teacher as the values
+        """
         for subject,teacher_details in sub.items():
-            if teacher_details['teacher'] == teacher_name:
+            if teacher_details['teacher'] == teacher_name: 
+                """
+                teacher_details['teacher']" prints the name of the teachers from the values of the dictionary "sub"
+                If the teacher name matches with the name passed as a parameter, then it adds the "name" part from the values of the same dictionary in an
+                empty list "books". Use set to remove duplicacy of the books taught by that teacher.
+                """
+                
                 books.append(teacher_details['name'])
                 books = list(set(books)) #remove string duplicacy
-    return books
-print(which_book_taught(courses, 'Dave'))
-
-def when_offered(courses, course):
-    semesters = []
-    for sem, sub in courses.items():
+                
         if course in sub.keys():
-            semesters.append(sem)
-    return semesters
-print(when_offered(courses, 'cs101'))
+            """
+            Checks if the course passed as a parameter is present in the keys of "sub" dictionary. If yes, then it adds the value of "sem", i.e., the key
+            of dictionary "courses" in an empty list, "semester".
+            """
+            semester.append(sem)
+            
+    return books, semester
+
+books, semester = which_book_taught_in_sem(courses, 'Dave', 'cs101')
+print("Dave teaches {0} in semester {1}".format(books, semester)) # We're checking what are the books taught and in which semester by the teacher named 'Dave' 
 
 
